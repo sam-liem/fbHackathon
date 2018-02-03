78a93bfce07c68b8a67c1e02664f7551ce1565d1
Tom Miller <M1LL3RB0Y@users.noreply.github.com>
2018-02-03 19:06:22 +0000

    widgetInPage stuff

--- js/widgetInPage.js
+++ js/widgetInPage.js
@@ -6,42 +6,38 @@ $(document).ready(function() {
 
 	function insertWidget() {
 		//get location to insert
-		var buttonRow = document.getElementsByClassName("_39bj");
-		var ourButton = document.createElement("li");
-		ourButton.innerHTML = "<a class=\"snippetParseButton\" role=\"button\" href=\"#\"></a>";
-	    buttonRow[0].appendChild(ourButton);
-	    console.log(buttonRow.length);
-	    //Appending to DOM 
+		var buttonRow = $("._39bj");
+		var ourButton = $("<li>").html("<a class=\"snippetParseButton\" role=\"button\" href=\"#\"></a>");
+	    buttonRow.prepend(ourButton);
+	    console.log("button added");
 	}
 
 	function beginWidget() {
 		if(active){
 			active = false;
 			console.log("deactivate");
-			var mainArea = $("_5irm _1_73");
-			mainArea.r
+			$(".dropup").remove();
 		}
 		else{
 			active = true;
 			console.log("activate");
-			var textBox = $("_1mf _1mj");
-			$("_5irm _1_73").innerHTML =
-				`<div class="dropdown">
-					<button class="btn btn-primary dropdown-toggle" id="menu1" type="button" data-toggle="dropdown">Dropdown Example
-					<span class="caret"></span></button>
-					<ul class="dropdown-menu" role="menu" aria-labelledby="menu1">
-					  <li role="presentation"><a role="menuitem" tabindex="-1" href="#">HTML</a></li>
-					  <li role="presentation"><a role="menuitem" tabindex="-1" href="#">CSS</a></li>
-					  <li role="presentation"><a role="menuitem" tabindex="-1" href="#">JavaScript</a></li>
-					  <li role="presentation" class="divider"></li>
-					  <li role="presentation"><a role="menuitem" tabindex="-1" href="#">About Us</a></li>    
-					</ul>
+			var textBox = $("._1mf._1mj");
+			$("._5irm._1_73").before(
+				`<div class="widgetMenu">
+					<form>
+						<label class="radio-inline"><input type="radio" name="optradio">Latex</label>
+						<label class="radio-inline"><input type="radio" name="optradio">Javascript</label>
+						<label class="radio-inline"><input type="radio" name="optradio">Python</label>
+					</form>
 				</div>`
+			)
+  			//$(".dropdown-toggle").dropdown("toggle");
 			console.log("prepended");
 		}
 	}
 
-	insertWidget();
+  	insertWidget();
+	
 
 	$(".snippetParseButton").on("click",function(){
     	console.log("pressed button");
