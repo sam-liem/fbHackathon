fcbd088b53d490924e43eaaf2813c29f30e64a16
PabiGamito <pablogamito@gmail.com>
2018-02-03 16:24:20 +0000

    updated preview-code

--- js/preview-code.js
+++ js/preview-code.js
@@ -14,10 +14,30 @@ console.log("Preview code loaded");
 
 $(document).ready(function() {
   console.log("Messenger ready");
-  console.log($("._5rpb").html());
+
   // To be called when language is selected
   function formatInputBox() {
     // const  inputText();
+  }
+
+  function format() {
+    var langTag = "js"
+    $("._5rpb").prepend( 
+      "<pre class='prettyprint linenums lang-" + langTag + "'>" + `
+        // Voila
+        static const string VOILA = "Voila";
+        function testFunction() {
+          return "Hello World!";
+        }
+        // This is just some more test code
+        testFunction();
+        var test = "Yo!";
+        console.log("Printing some cool stuff");
+
+        // This should be the last line of code!
+      </pre>`
+    )
+    // PR.prettyPrint();
   }
 
   // Returns an array on input text one line for each input
@@ -33,6 +53,7 @@ $(document).ready(function() {
   $("._5rpb").on("DOMSubtreeModified",function(){
     console.log("Change in input detected");
     inputText();
+    format();
   });
 
 });
