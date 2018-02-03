ac8a320f9b950b61664391ebed92686ce2816ba1
PabiGamito <pablogamito@gmail.com>
2018-02-03 17:02:29 +0000

    remove infinite loop

--- js/preview-code.js
+++ js/preview-code.js
@@ -14,33 +14,43 @@ console.log("Preview code loaded");
 
 $(document).ready(function() {
   console.log("Messenger ready");
+  initialize();
 
   // To be called when language is selected
   function formatInputBox() {
     // const  inputText();
   }
 
-  function format() {
-    var langTag = "js"
-    var code = `
-        // Voila
-        static const string VOILA = "Voila";
-        function testFunction() {
-          return "Hello World!";
-        }
-        // This is just some more test code
-        testFunction();
-        var test = "Yo!";
-        console.log("Printing some cool stuff");
-
-        // This should be the last line of code!
-      `
-
+  function initialize() {
     var pre = document.createElement('pre');
+    pre.setAttribute("id", "code-preview");
     pre.className = "prettyprint prettyprinted";
-    var formattedCode = PR.prettyPrintOne(code, 'py', true);
-    pre.innerHTML = formattedCode;
     $('._5rpb').prepend(pre);
+  }
+
+  function format(inputText) {
+    var langTag = "js"
+    // var code = `
+    //     // Line
+    //     static const string VOILA = "Voila";
+    //     function testFunction() {
+    //       return "Hello World!";
+    //     }
+    //     // This is just some more test code
+    //     testFunction();
+    //     var test = "Yo!";
+    //     console.log("Printing some cool stuff");
+
+    //     // This should be the last line of code!
+    //   `
+
+    for (var i = 0; i < inputText.length; i++) {
+      console.log(inputText[i]);
+    }
+
+    var pre = $("._5rpb #code-preview");
+    var formattedCode = PR.prettyPrintOne(code, 'py', true);
+    pre.html(formattedCode);
   }
 
   // Returns an array on input text one line for each input
@@ -53,10 +63,9 @@ $(document).ready(function() {
     return inputs;
   }
 
-  $("._5rpb").on("DOMSubtreeModified",function(){
+  $("._5rpb div").on("DOMSubtreeModified",function(){
     // console.log("Change in input detected");
-    inputText();
-    format();
+    format(inputText());
   });
 
 });
