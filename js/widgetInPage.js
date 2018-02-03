537744cfdb9fafbef747256ec767d3c30d9ef61e
Tom Miller <M1LL3RB0Y@users.noreply.github.com>
2018-02-03 20:43:52 +0000

    forgot file

--- js/widgetInPage.js
+++ js/widgetInPage.js
@@ -6,17 +6,25 @@ $(document).ready(function() {
 
 	function insertWidget() {
 		//get location to insert
+		var iconImg = chrome.extension.getURL('images/logo_icon.png');
 		var buttonRow = $("._39bj");
-		var ourButton = $("<li>").html("<a class=\"snippetParseButton\" role=\"button\" href=\"#\"></a>");
+		var ourButton = $("<li>").html(
+			"<a class=\"snippetParseButton\" role=\"button\" href=\"#\" style=\"background-image:url(\'" +iconImg+ "\')\"></a>"
+		);
 	    buttonRow.prepend(ourButton);
 	    console.log("button added");
+
+		$(".snippetParseButton").on("click",function(){
+	    	console.log("pressed button");
+	    	beginWidget();
+	  	});
 	}
 
 	function beginWidget() {
 		if(active){
 			active = false;
 			console.log("deactivate");
-			$(".dropup").remove();
+			$(".widgetMenu").remove();
 		}
 		else{
 			active = true;
@@ -24,23 +32,31 @@ $(document).ready(function() {
 			var textBox = $("._1mf._1mj");
 			$("._5irm._1_73").before(
 				`<div class="widgetMenu">
-					<form>
-						<label class="radio-inline"><input type="radio" name="optradio">Latex</label>
-						<label class="radio-inline"><input type="radio" name="optradio">Javascript</label>
-						<label class="radio-inline"><input type="radio" name="optradio">Python</label>
+					<form class="widgetMenuForm">
+						<label class="radio-inline"><input type="radio" name="optradio" id="latex">Latex</label>
+						<label class="radio-inline"><input type="radio" name="optradio" id="js">Javascript</label>
+						<label class="radio-inline"><input type="radio" name="optradio" id="py">Python</label>
+						<label class="radio-inline"><input type="radio" name="optradio" id="cpp">C++</label>
+						<label class="radio-inline"><input type="radio" name="optradio" id="raw">Raw Text</label>
 					</form>
 				</div>`
 			)
-  			//$(".dropdown-toggle").dropdown("toggle");
+
+			$('.widgetMenuForm').change(function(){
+            	var selected = $("input[name='optradio']:checked").attr('id');
+            	console.log(selected);
+    		});
 			console.log("prepended");
+			//call preview-code
+			//initialize()
 		}
 	}
 
   	insertWidget();
 	
-
-	$(".snippetParseButton").on("click",function(){
-    	console.log("pressed button");
-    	beginWidget();
-  	});
+	$(window).resize(function() {
+  		if(!$(".snippetParseButton")[0]){
+  			insertWidget()
+  		}
+	});
 });
