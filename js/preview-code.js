a159393cbff0954aa9c46a254a2773cc579886b6
shl3617 <shl3617@ic.ac.uk>
2018-02-04 00:25:07 +0000

    pushed current progress

--- js/preview-code.js
+++ js/preview-code.js
@@ -21,17 +21,105 @@ var numberOfLines = 0;
 // }
 
 $(document).ready(function() {
-  console.log("Messenger ready");
-  initialize();
 
+  var active = false;
+  var selected = "";
+
+    function insertWidget() {
+        //get location to insert
+        var iconImg = chrome.extension.getURL('images/icon.png');
+        var buttonRow = $("._39bj");
+        var ourButton = $("<li></li>").html(
+            "<a class=\"snippetParseButton\" role=\"button\" href=\"#\" style=\"background-image:url(\'" +iconImg+ "\')\"></a>"
+        );
+        
+        buttonRow.prepend(ourButton);
+        console.log("button added");
+
+        $(".snippetParseButton").on("click",function(){
+            console.log("pressed button");
+            beginWidget();
+        });
+    }
+
+    function beginWidget() {
+        if(active){
+            active = false;
+            console.log("deactivate");
+            $(".widgetMenu").remove();
+            removeCodeEditor();
+        } else {
+            active = true;
+            console.log("activate");
+            var textBox = $("._1mf._1mj");
+            $("._5irm").before(
+                `<div class="widgetMenu">
+                    <form class="widgetMenuForm">
+                        <label class="radio-inline"><input type="radio" name="optradio" id="latex" checked>Latex</label>
+                        <label class="radio-inline"><input type="radio" name="optradio" id="js">Javascript</label>
+                        <label class="radio-inline"><input type="radio" name="optradio" id="py">Python</label>
+                        <label class="radio-inline"><input type="radio" name="optradio" id="cpp">C++</label>
+                        <label class="radio-inline"><input type="radio" name="optradio" id="raw">Raw Text</label>
+                    </form>
+                </div>`
+            );
+            selected = 'latex';
+            $('.widgetMenuForm').append("<a class=\"widgetMenuPreview\" role=\"button\" href=\"#\">Preview ON/OFF</a>")
+            $(".widgetMenuPreview").on("click",function(){
+                console.log("toggled preview");
+                //togglePreview();
+            });
+
+            $('.widgetMenuForm').change(function(){
+                selected = $("input[name='optradio']:checked").attr('id');
+                if(selected=='latex'){
+                    $(".widgetMenuPreview").show();
+                }else{
+                    $(".widgetMenuPreview").hide();
+                }
+                console.log(selected);
+                //changeInput(selected);
+            });
+            console.log("prepended");
+            //call preview-code
+            initialize();
+        }
+    }
+
+    insertWidget();
+    
+    $(window).resize(function() {
+        if(!$(".snippetParseButton")[0]){
+            insertWidget()
+        }
+    });
+
+  console.log("Messenger ready");
+  // initialize();
   // To be called when language is selected
   function formatInputBox() {
     // const  inputText();
   }
 
+  function removeCodeEditor() {
+    $('._5rpu').first().css({opacity: 1,paddingLeft:"1.5%"});
+    $('#code-preview').css({display: "none"});
+    $(".blinking-cursor").first().css({display: "none"});
+    $("._4_j4").first().css({marginBottom: "-10"});
+    $('._5irm').first().css({marginLeft:"12px"});
+    var enterField = document.getElementsByClassName('_5irm')[0];
+    enterField.style.marginLeft = "2px";
+    enterField.firstChild.style.backgroundColor = "#ffff";
+    enterField.firstChild.style.marginRight = "1%";
+  }
+
   // Must always run on document load for it to work
   function initialize() {
-    $('._5rpu').css({opacity: 0});
+    $('._5rpu').css({opacity: 0,paddingLeft: "42px"});
+    var enterField = document.getElementsByClassName('_5irm')[0];
+    enterField.style.marginLeft = "0px";
+    enterField.firstChild.style.backgroundColor = "#303640";
+    enterField.firstChild.style.marginRight = "1%";
     var pre = document.createElement('pre');
     pre.setAttribute("id", "code-preview");
     pre.className = "prettyprint prettyprinted";
@@ -40,8 +128,10 @@ $(document).ready(function() {
     cursor.innerHTML = '|'
     $('._5rpb').prepend(pre);
     $('._5rpb').prepend(cursor);
-    format(inputText(), 'js');
+    format(inputText(), selected);
     initializeCursor();
+
+    // removeCodeEditor();
   }
 
   function initializeCursor() {
@@ -57,7 +147,6 @@ $(document).ready(function() {
     for (var i = 0; i < inputText.length; i++) {
       code += inputText[i] + "\n";
     }
-
     var pre = $("#code-preview");
     var formattedCode = PR.prettyPrintOne(code, lang, true);
     pre.html(formattedCode);
@@ -74,7 +163,7 @@ $(document).ready(function() {
   }
 
   $("._5rpu").on("DOMSubtreeModified",function(){
-    format(inputText(), 'js');
+    format(inputText(), selected);
     // Checks if letter was added or remove
     var inputLines = $('._1mf._1mj');
     var newCursorLine = cursorLine;
@@ -116,11 +205,6 @@ $(document).ready(function() {
     setCursorPos(cursorLine, cursorLetter);
 
   });
-
-  var enterField = document.getElementsByClassName('_5irm')[0];
-  enterField.style.marginLeft = "0px";
-  enterField.firstChild.style.backgroundColor = "#303640";
-  enterField.firstChild.style.marginRight = "1%";
 
   $('._5rpu').click(function (e) { //Offset mouse Position
     var posX = $(this).offset().left,
@@ -183,11 +267,13 @@ $(document).ready(function() {
   });
 
   $(document).on('click', function(e) {
-    var elem = document.getElementsByClassName('_4_j4')[0];
-    if (e.target == elem || elem.contains(e.target)) {
-      $('.blinking-cursor').show();
-    } else {
-      $('.blinking-cursor').hide();
+    if($('.blinking-cursor').first().is(':visible')) {
+      var elem = document.getElementsByClassName('_4_j4')[0];
+      if (e.target == elem || elem.contains(e.target)) {
+        $('.blinking-cursor').show();
+      } else {
+        $('.blinking-cursor').hide();
+      }
     }    
   });
 
--- js/widgetInPage.js
+++ js/widgetInPage.js
@@ -2,74 +2,73 @@
 console.log("testtt")
 
 $(document).ready(function() {
-	var active = false;
+    var active = false;
 
-	function insertWidget() {
-		//get location to insert
-		var iconImg = chrome.extension.getURL('images/icon.png');
-		var buttonRow = $("._39bj");
-		var ourButton = $("<li></li>").html(
-			"<a class=\"snippetParseButton\" role=\"button\" href=\"#\" style=\"background-image:url(\'" +iconImg+ "\')\"></a>"
-		);
-		
-		buttonRow.prepend(ourButton);
-		console.log("button added");
+    function insertWidget() {
+        //get location to insert
+        var iconImg = chrome.extension.getURL('images/icon.png');
+        var buttonRow = $("._39bj");
+        var ourButton = $("<li></li>").html(
+            "<a class=\"snippetParseButton\" role=\"button\" href=\"#\" style=\"background-image:url(\'" +iconImg+ "\')\"></a>"
+        );
+        
+        buttonRow.prepend(ourButton);
+        console.log("button added");
 
-		$(".snippetParseButton").on("click",function(){
-			console.log("pressed button");
-			beginWidget();
-		});
-	}
+        $(".snippetParseButton").on("click",function(){
+            console.log("pressed button");
+            beginWidget();
+        });
+    }
 
-	function beginWidget() {
-		if(active){
-			active = false;
-			console.log("deactivate");
-			$(".widgetMenu").remove();
-		}
-		else{
-			active = true;
-			console.log("activate");
-			var textBox = $("._1mf._1mj");
-			$("._5irm").before(
-				`<div class="widgetMenu">
-					<form class="widgetMenuForm">
-						<label class="radio-inline"><input type="radio" name="optradio" id="latex" checked>Latex</label>
-						<label class="radio-inline"><input type="radio" name="optradio" id="js">Javascript</label>
-						<label class="radio-inline"><input type="radio" name="optradio" id="py">Python</label>
-						<label class="radio-inline"><input type="radio" name="optradio" id="cpp">C++</label>
-						<label class="radio-inline"><input type="radio" name="optradio" id="raw">Raw Text</label>
-					</form>
-				</div>`
-			);
-			var selected = 'latex';
-			$('.widgetMenuForm').append("<a class=\"widgetMenuPreview\" role=\"button\" href=\"#\">Preview ON/OFF</a>")
-			$(".widgetMenuPreview").on("click",function(){
-				console.log("toggled preview");
-				//togglePreview();
-			});
+    function beginWidget() {
+        if(active){
+            active = false;
+            console.log("deactivate");
+            $(".widgetMenu").remove();
+        } else {
+            active = true;
+            console.log("activate");
+            var textBox = $("._1mf._1mj");
+            $("._5irm").before(
+                `<div class="widgetMenu">
+                    <form class="widgetMenuForm">
+                        <label class="radio-inline"><input type="radio" name="optradio" id="latex" checked>Latex</label>
+                        <label class="radio-inline"><input type="radio" name="optradio" id="js">Javascript</label>
+                        <label class="radio-inline"><input type="radio" name="optradio" id="py">Python</label>
+                        <label class="radio-inline"><input type="radio" name="optradio" id="cpp">C++</label>
+                        <label class="radio-inline"><input type="radio" name="optradio" id="raw">Raw Text</label>
+                    </form>
+                </div>`
+            );
+            var selected = 'latex';
+            $('.widgetMenuForm').append("<a class=\"widgetMenuPreview\" role=\"button\" href=\"#\">Preview ON/OFF</a>")
+            $(".widgetMenuPreview").on("click",function(){
+                console.log("toggled preview");
+                //togglePreview();
+            });
 
-			$('.widgetMenuForm').change(function(){
-				selected = $("input[name='optradio']:checked").attr('id');
-				if(selected=='latex'){
-					$(".widgetMenuPreview").show();
-				}else{
-					$(".widgetMenuPreview").hide();
-				}
-				console.log(selected);
-				//changeInput(selected);
-			});
-			console.log("prepended");
-			//call preview-code
-			//initialize()
-		}
-	}
+            $('.widgetMenuForm').change(function(){
+                selected = $("input[name='optradio']:checked").attr('id');
+                if(selected=='latex'){
+                    $(".widgetMenuPreview").show();
+                }else{
+                    $(".widgetMenuPreview").hide();
+                }
+                console.log(selected);
+                //changeInput(selected);
+            });
+            console.log("prepended");
+            //call preview-code
+            //initialize();
+        }
+    }
 
-	insertWidget();
-	
-	$(window).resize(function() {
-		if(!$(".snippetParseButton")[0]){
-			insertWidget()
-		}
-	});
+    insertWidget();
+    
+    $(window).resize(function() {
+        if(!$(".snippetParseButton")[0]){
+            insertWidget()
+        }
+    });
 });
